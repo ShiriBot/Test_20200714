9875b9cd5607fd9511c7238d05e109fa8759a5c2
채정실 <cowjdtlf@gmail.com>
2020-07-14 20:42:57 +0900

    1. fragment 레이아웃 원하는 숫자만큼 만들기(xml) 2. Fragment 클래스 갯수 맞춰서 만들고 레이아웃과 연결하기 3. 어댑터 만들기. 포지션 변수별로 분기처리. 4. 메인액티비티에 어댑터연결.

--- app/src/main/java/kr/co/tjoeun/test_20200714/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/test_20200714/MainActivity.kt
@@ -2,12 +2,17 @@ package kr.co.tjoeun.test_20200714
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.test_20200714.adapters.MainViewPagerAdapter
 
 class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        SetValues()
+        SetupEvents()
     }
 
     override fun SetValues() {
@@ -15,6 +20,11 @@ class MainActivity : BaseActivity() {
     }
 
     override fun SetupEvents() {
+
+        val mvpa = MainViewPagerAdapter(supportFragmentManager)
+        viewPager.adapter=mvpa
+
+        tabLayout.setupWithViewPager(viewPager)
 
     }
 
---
+++ app/src/main/java/kr/co/tjoeun/test_20200714/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,28 @@
+package kr.co.tjoeun.test_20200714.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import kr.co.tjoeun.test_20200714.fragment.FirstFragment
+import kr.co.tjoeun.test_20200714.fragment.SecondFragment
+
+class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position){
+            0->"피자 주문"
+            else->"내 정보 설정"
+        }
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> FirstFragment()
+            else -> SecondFragment()
+        }
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+}
